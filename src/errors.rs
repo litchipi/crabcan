47bfa5f5fa581dc857f665638ee8c62d0c33c5be
litchipi <litchi.pi@protonmail.com>
2021-09-15 09:58:16 +0200

    step 4: Validate arguments parsed
    Signed-off-by: litchipi <litchi.pi@protonmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -25,6 +25,10 @@ impl fmt::Display for Errcode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // Define what behaviour for each variant of the enum
         match &self{
+
+            // Message to display when an argument is invalid
+            Errcode::ArgumentInvalid(element) => write!(f, "ArgumentInvalid: {}", element),
+
             _ => write!(f, "{:?}", self) // For any variant not previously covered
         }
     }

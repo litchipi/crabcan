f083d3043b95165ae948e6f408ba9ac598ea7e3b
litchipi <litchi.pi@protonmail.com>
2021-09-15 09:56:21 +0200

    step 3: Setup errors handling
    Signed-off-by: litchipi <litchi.pi@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::process::exit;
+
+mod errors;
 mod cli;
 
+use errors::exit_with_retcode;
+
 fn main() {
-    let args = cli::parse_args();
-    log::info!("{:?}", args);
+    match cli::parse_args(){
+        Ok(args) => {
+            log::info!("{:?}", args);
+            exit_with_retcode(Ok(()))
+        },
+        Err(e) => {
+            log::error!("Error while parsing arguments:\n\t{}", e);
+            exit(e.get_retcode());
+        }
+    };
 }

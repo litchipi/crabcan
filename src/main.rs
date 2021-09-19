97bdf56d6e2bad9152667815d5adee5eb9dec424
litchipi <litchi.pi@protonmail.com>
2021-09-19 14:25:58 +0200

    step5: Create the skeletton on the container software
    Signed-off-by: litchipi <litchi.pi@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::process::exit;
 
 mod errors;
 mod cli;
+mod container;
+mod config;
 
 use errors::exit_with_retcode;
 
@@ -9,7 +11,7 @@ fn main() {
     match cli::parse_args(){
         Ok(args) => {
             log::info!("{:?}", args);
-            exit_with_retcode(Ok(()))
+            exit_with_retcode(container::start(args))
         },
         Err(e) => {
             log::error!("Error while parsing arguments:\n\t{}", e);

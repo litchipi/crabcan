833b99cb1acf051cffc418d6ad205a588e3e4919
Litchi Pi <litchi.pi@proton.me>
2025-01-14 11:02:39 +0100

    Fixup argument parsing, update dependencies
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- src/cli.rs
+++ src/cli.rs
@@ -1,29 +1,29 @@
 use std::path::PathBuf;
-use structopt::StructOpt;
+use clap::{arg, Parser};
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "crabcan", about = "A simple container in Rust.")]
+#[derive(Parser, Debug)]
+#[command(name = "crabcan", about = "A simple container in Rust.")]
 pub struct Args {
     /// Activate debug mode
     // short and long flags (-d, --debug) will be deduced from the field's name
-    #[structopt(short, long)]
+    #[arg(short, long)]
     debug: bool,
 
     /// Command to execute inside the container
-    #[structopt(short, long)]
+    #[arg(short, long)]
     pub command: String,
 
     /// User ID to create inside the container
-    #[structopt(short, long)]
+    #[arg(short, long)]
     pub uid: u32,
 
     /// Directory to mount as root of the container
-    #[structopt(parse(from_os_str), short = "m", long = "mount")]
+    #[arg(short = 'm', long = "mount")]
     pub mount_dir: PathBuf,
 }
 
 pub fn parse_args() -> Args {
-    let args = Args::from_args();
+    let args = Args::parse();
 
     if args.debug{
         setup_log(log::LevelFilter::Debug);
